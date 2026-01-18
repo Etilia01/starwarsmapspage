da5d304a3188fd45d6fa5b1fc8d61bb576bbee9e
Etilia <alexandridisjulia@gmail.com>
2026-01-18 13:46:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const Planet1 = "O:6";
+const Planet1alt = "o:6";
+
+
+function checkmaps(){
+    const planetinput = document.getElementById("planetinput").value;
+    if (planetinput== Planet1){
+        window.location.href = "dathomir.html"
+    }
+    if (planetinput== Planet1alt){
+        window.location.href = "dathomir.html"
+    }
+}
