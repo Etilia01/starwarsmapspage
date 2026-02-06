34892f88962d49827b42d44f652d74d57db2cdf1
Etilia <alexandridisjulia@gmail.com>
2026-02-06 15:22:59 +0000

    added bonadan

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const Planet1 = "O:6";
 const Planet1alt = "o:6";
-
+const Planet2 = "S:3";
+const Planet2alt = "s:3";
 
 function checkmaps(){
     const planetinput = document.getElementById("planetinput").value;
@@ -9,5 +10,11 @@ function checkmaps(){
     }
     if (planetinput== Planet1alt){
         window.location.href = "dathomir.html"
+    }
+    if (planetinput== Planet2){
+        window.location.href = "bonadan.html"
+    }
+    if (planetinput== Planet2alt){
+        window.location.href = "bonadan.html"
     }
 }
